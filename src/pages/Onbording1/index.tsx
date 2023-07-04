d4a892acaa463f5379be6a3c6fabb4c722055501
raphael-caninde <raphael.caninde@gmail.com>
2023-07-03 23:31:47 -0300

    feat: add page onbording1

---
+++ src/pages/Onbording1/index.tsx
@@ -0,0 +1,52 @@
+import { useNavigation, useTheme } from '@react-navigation/native';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  ImageBackground,
+} from 'react-native';
+
+import { ScreenIndicators } from '../../components/ScreenIndicators';
+
+import voltar from '../../assets/Voltar.png';
+import background from '../../assets/Backgroundonbording.png';
+import study from '../../assets/EstudarOnbording.png';
+
+import styles from './styles';
+
+export function Onbording1() {
+  const navigation = useNavigation<any>();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerTop}>
+        <ImageBackground
+          style={styles.imgback}
+          source={background}
+          resizeMode='cover'
+        >
+          <Image source={study} />
+        </ImageBackground>
+      </View>
+      <View style={styles.mainContainer}>
+        <View style={styles.containerText}>
+          <Text style={styles.number}>01.</Text>
+          <Text style={styles.text}>
+            Encontre vários professores para ensinar você
+          </Text>
+        </View>
+        <View style={styles.nextButton}>
+          <ScreenIndicators
+            count={2}
+            activeIndex={0}
+            color={'#8257E5'}
+          />
+          <TouchableOpacity onPress={() => navigation.navigate('screen2')}>
+            <Image source={voltar} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ src/pages/Onbording1/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  containerTop: {
+    backgroundColor: '#8257E5',
+    height: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#EBEBF5',
+    padding: 30,
+  },
+
+  imgback: {
+    width: 234,
+    height: 220,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  containerText: {
+    justifyContent: 'center',
+    marginVertical: 50,
+  },
+
+  number: {
+    color: '#6A6180',
+    fontSize: 40,
+    lineHeight: 43,
+    opacity: 0.2,
+  },
+
+  text: {
+    color: '#6A6180',
+    fontSize: 24,
+    lineHeight: 34,
+    width: 220,
+    fontFamily: 'Poppins_500Medium',
+    marginTop: 40,
+  },
+
+  nextButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+});
+
+export default styles;
