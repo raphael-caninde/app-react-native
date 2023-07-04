a2e7928f45f93272a9eb37d53822af6e2cfd9d50
raphael-caninde <raphael.caninde@gmail.com>
2023-07-03 23:33:12 -0300

    feat:  onbording indicators

---
+++ src/components/ScreenIndicators/index.tsx
@@ -0,0 +1,38 @@
+import { useTheme } from '@react-navigation/native';
+import { View } from 'react-native';
+
+interface ScreenIndicatorsProps {
+  count: number;
+  activeIndex: number;
+  color: string;
+}
+
+export function ScreenIndicators({
+  count,
+  activeIndex,
+  color
+}: ScreenIndicatorsProps) {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 8,
+        marginVertical: 32,
+      }}
+    >
+      {new Array(count).fill('1').map((_, i) => (
+        <View
+          style={{
+            width: 8,
+            height: 8,
+            borderRadius: 8,
+            backgroundColor: i === activeIndex ? color : '#C1BCCC',
+          }}
+          key={i}
+        />
+      ))}
+    </View>
+  );
+}
